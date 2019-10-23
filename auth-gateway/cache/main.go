dde0e70754c3ce7c53330293f20b287f24205392
Xiangrui2019 <xiangrui@aiursoft.com>
2019-10-23 20:15:22 +0800

    add cache

--- auth-gateway/cache/main.go
+++ auth-gateway/cache/main.go
@@ -1,7 +1,16 @@
 package cache
 
+import (
+	"github.com/astaxie/beego"
+	"github.com/go-redis/redis"
+)
+
 var RedisClient *redis.Client
 
 func ConnectRedisCache() {
-	RedisClient = nil
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr:     beego.AppConfig.String("redis_addr"),
+		Password: beego.AppConfig.String("redis_password"),
+		DB:       beego.AppConfig.DefaultInt("redis_db", 0),
+	})
 }
